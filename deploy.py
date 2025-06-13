24b1f7fd34199f4a29fe373fdff4cd69de5d2750
fabiofalopes <fabiofalopes@gmail.com>
2025-06-13 18:36:49 +0100

    sync

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
-from nicegui import app, ui
+from nicegui import ui
 
 import main
 
 main.create()
-ui.export('static') 
+
+ui.run(just_build='pyscript', storage_secret='a_random_and_long_secret_string_for_testing') 
